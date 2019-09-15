5722d947c40914afcefb0adde1153164eac3bbc9
Takahashi Tomohiko <takahashi@tomohiko.io>
2019-09-15 21:17:11 +0900

    watcher

--- index.js
+++ index.js
@@ -99,6 +99,37 @@ var app9 = new Vue({
   }
 })
 
-
-
+var app10 = new Vue({
+  el: '#app-10',
+  data: {
+    question: '',
+    answer: 'I cannot give you an answer until you ask a question!'
+  },
+  created: function() {
+    this.debouncedGetAnswer = _.debounce(this.getAnswer, 500)
+  },
+  watch: {
+    question: function (newQuestion, oldQuestion) {
+      this.answer = 'Waiting for you to stop typing...'
+      this.debouncedGetAnswer()
+    }
+  },
+  methods: {
+    getAnswer: function() {
+      if (this.question.indexOf('?') === -1) {
+        this.answer = 'Questions usually contain a question mark.'
+        return
+      }
+      this.answer = 'Thinking...'
+      var vm = this
+      axios.get('https://yesno.wtf/api')
+      .then(function (response) {
+        vm.answer = _.capitalize(response.data.answer)
+      })
+      .catch(function (error) {
+        vm.answer = 'Error! Could not reach the API. ' + error
+      })
+    }
+  }
+})
 
